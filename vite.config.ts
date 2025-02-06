8388fe3c30ae1dd3b8e7114dec0881a2ead4dabd
Louis <p19970408@gmail.com>
2025-02-06 10:23:13 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     vue(),
     vueDevTools(),
+    legacy({
+      targets: ['defaults', 'IE11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
